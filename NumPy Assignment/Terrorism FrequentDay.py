beb5d9215b403f6d1011fd7b3eb830b5bdeb624b
Evina Novi Thekkudan <88366889+evinatn@users.noreply.github.com>
2022-01-16 17:04:20 +0530

    Create Terrorism FrequentDay.py

---
+++ NumPy Assignment/Terrorism FrequentDay.py
@@ -0,0 +1,13 @@
+## Open and read data file as specified in the question
+## Print the required output in given format
+import csv
+import numpy as np
+with open("terrorismData.csv",encoding="utf8") as file_obj:
+    file_data=csv.DictReader(file_obj,skipinitialspace=True)
+    attack=[]
+    for row in file_data:
+        attack.append(row['Day'])
+    attack=np.array(attack,dtype=int)
+    a,counts=np.unique(attack,return_counts=True)
+    x=counts.argmax()
+    print(a[x],counts[x])
